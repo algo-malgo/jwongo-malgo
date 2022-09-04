8b992dcb6e26c4bfeca0ab939f82b7e4201bd5ac
jynam <njy1007@gmail.com>
2022-09-05 00:45:33 +0900

    정답

--- BOJ/11725 트리의 부모 찾기/main.js
+++ BOJ/11725 트리의 부모 찾기/main.js
@@ -1,17 +1,44 @@
 const solution = (edges) => {
-  const tree = [null, { parent: null, degree: 1 }];
+  const tree = new Map();
   edges.forEach((edge) => {
-    let parent = edge[0];
-    let child = edge[1];
-    if (tree[edge[1]]) [parent, child] = [child, parent];
-    tree[child] = { parent: parent, degree: tree[parent].degree + 1 };
+    tree.get(edge[0])
+      ? tree.get(edge[0]).push(edge[1])
+      : tree.set(edge[0], [edge[1]]);
+    tree.get(edge[1])
+      ? tree.get(edge[1]).push(edge[0])
+      : tree.set(edge[1], [edge[0]]);
   });
-  console.log(
-    tree.reduce((acc, curr) =>
-      curr && curr.parent ? acc.concat(`${curr.parent}\n`) : acc
-    ),
-    ''
-  );
+
+  const degrees = Array(edges.length + 1).fill(-1);
+  let degree = 1;
+
+  const visited = Array(edges.length + 1).fill(false);
+  const queue = [];
+  visited[1] = true;
+  queue.push(1);
+  degrees[1] = degree;
+  while (queue.length) {
+    degree++;
+    const front = queue.shift();
+    tree.get(front) &&
+      tree.get(front).forEach((vertex) => {
+        if (!visited[vertex]) {
+          visited[vertex] = true;
+          queue.push(vertex);
+          degrees[vertex] = degree;
+        }
+      });
+  }
+
+  const answer = Array(edges.length + 1).fill(-1);
+  edges.forEach((edge) => {
+    if (degrees[edge[0]] > degrees[edge[1]]) answer[edge[0]] = edge[1];
+    else answer[edge[1]] = edge[0];
+  });
+
+  answer.shift();
+  answer.shift();
+  console.log(answer.join('\n'));
 };
 
 const fs = require('fs');
