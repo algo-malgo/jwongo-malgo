faacba52f250933798a31ed1606fe7335db6fe7b
jynam <njy1007@gmail.com>
2022-09-08 22:28:38 +0900

    Revert "성능 개선 시도 - 실패"
    This reverts commit 51635f495961a38b91271651db28d5771cd07cda.

--- BOJ/24479 알고리즘 수업 - 깊이 우선 탐색 1/main.js
+++ BOJ/24479 알고리즘 수업 - 깊이 우선 탐색 1/main.js
@@ -14,9 +14,9 @@ const solution = (N, R, edges) => {
     cnt++;
     answer[vertex] = cnt;
     visited[vertex] = true;
-    for (let i = 0; i < graph[vertex].length; i++) {
-      visited[graph[vertex][i]] || dfs(graph[vertex][i]);
-    }
+    graph[vertex].forEach((next) => {
+      visited[next] || dfs(next);
+    });
   };
   dfs(R);
   return answer.slice(1).join('\n');
