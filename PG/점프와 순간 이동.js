376ad7b95c5aa5020cbc5232cf15d32eff3f27a5
jynam <njy1007@gmail.com>
2022-09-17 11:17:29 +0900

    정답

--- PG/점프와 순간 이동.js
+++ PG/점프와 순간 이동.js
@@ -1,10 +1,12 @@
 function solution(n) {
-  const dp = [0, 1];
-
-  for (let i = 2; i <= n; i++) {
-    if (i % 2 === 0) dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2]);
-    else dp[i] = dp[i - 1] + 1;
+  let answer = 0;
+  while (n > 0) {
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n--;
+      answer++;
+    }
   }
-
-  return dp[n];
+  return answer;
 }
