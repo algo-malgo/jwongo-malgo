91962f4d176dfde7a871a0eda1b9d88ff3346cb3
jynam <njy1007@gmail.com>
2022-09-24 11:41:37 +0900

    정답

--- PG/n^2 배열 자르기.js
+++ PG/n^2 배열 자르기.js
@@ -1,10 +1,11 @@
 function solution(n, left, right) {
-  const answer = [1];
-  for (let i = 2n; i <= n; i = i + 1n) {
-    for (let j = 0n; j < i; j = j + 1n) {
-      answer[BigInt(n) * j + (i - 1n)] = i;
-      answer[BigInt(n) * (i - 1n) + j] = i;
-    }
+  const answer = [];
+
+  for (let i = left; i <= right; i++) {
+    const x = Math.floor(i / n);
+    const y = i % n;
+    answer.push(Math.max(x, y) + 1);
   }
-  return answer.slice(left, right + 1);
+
+  return answer;
 }
