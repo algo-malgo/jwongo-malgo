0bc51333637df973a59b290414cbeeb9b889a4ef
jynam <njy1007@gmail.com>
2023-05-06 15:37:34 +0900

    정답

--- PG/가장 큰 정사각형 찾기.js
+++ PG/가장 큰 정사각형 찾기.js
@@ -1,40 +1,12 @@
 function solution(board) {
-  const visited = Array.from(Array(board.length), () => Array(board[0].length).fill(false));
-
-  const getSquareSize = (x, y) => {
-    let sideLength = 1;
-    while (x + sideLength < board.length && y + sideLength < board[0].length) {
-      let isSquare = true;
-
-      for (let i = 0; i < sideLength; i++) {
-        visited[x + sideLength][y + i] = true;
-        visited[x + i][y + sideLength] = true;
-        if (board[x + sideLength][y + i] === 0 || board[x + i][y + sideLength] === 0) {
-          isSquare = false;
-          break;
-        }
-      }
-      visited[x + sideLength][y + sideLength] = true;
-      if (board[x + sideLength][y + sideLength] === 0) {
-        isSquare = false;
-      }
-
-      if (isSquare) {
-        sideLength++;
-      } else {
-        break;
-      }
-    }
-    return sideLength * sideLength;
-  };
-
-  let answer = -1;
+  let answer = 0;
   board.forEach((row, x) => {
     row.forEach((_, y) => {
-      if (board[x][y] === 1 && !visited[x][y]) {
-        answer = Math.max(getSquareSize(x, y), answer);
+      if (board[x][y] > 0 && board[x - 1] && board[x - 1][y - 1]) {
+        board[x][y] = Math.min(board[x - 1][y], board[x - 1][y - 1], board[x][y - 1]) + 1;
       }
+      answer = Math.max(board[x][y], answer);
     });
   });
-  return answer;
+  return answer * answer;
 }
